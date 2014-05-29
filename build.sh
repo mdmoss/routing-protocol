aa105a62201de554ce47df5b92960a890b422ae2
Matthew Moss <mdm@cse.unsw.edu.au>
2014-05-29 14:26:47 +1000

    Add some scripts to make life easier

---
+++ build.sh
@@ -0,0 +1,1 @@
+javac -d . -source 6 src/*.java
---
+++ test.sh
@@ -0,0 +1,6 @@
+java dv_routing A 2000 configs/configA.txt &
+java dv_routing B 2001 configs/configB.txt &
+java dv_routing C 2002 configs/configC.txt &
+java dv_routing D 2003 configs/configD.txt &
+java dv_routing E 2004 configs/configE.txt &
+java dv_routing F 2005 configs/configF.txt 
